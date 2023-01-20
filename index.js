779fe8143cda764ca0a483159a4381fa47a90f46
Kaneknah <jamesnbaxley@protonmail.com>
2023-01-19 19:24:56 -0700

    index.js started

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const Employy = require("./lib/employee");
+const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
+
+const fs = require("fs");
+
+
+
+
+let htmlContent = `<html>
+
+
+
+
+
+</html>`;
+
+fs.writeFile("./my_team.html", htmlContent, (error) => {
+	console.log(error);
+});
